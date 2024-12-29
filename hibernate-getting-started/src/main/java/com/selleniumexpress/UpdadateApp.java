4c717743226de858c19eb184d9b583b9f8e9cea8
anand <anand1168@gmail.com>
2024-12-29 15:00:12 +0530

    changes done
    Signed-off-by: anand <anand1168@gmail.com>

--- hibernate-getting-started/src/main/java/com/selleniumexpress/UpdadateApp.java
+++ hibernate-getting-started/src/main/java/com/selleniumexpress/UpdadateApp.java
@@ -7,44 +7,34 @@ import org.hibernate.cfg.Configuration;
 import com.selleniumexpress.entity.Song;
 import com.selleniumexpress.utils.Utility;
 
-public class UpdadateApp 
-{
-    public static void main( String[] args )
-    {
-    	SessionFactory sessionFactory=Utility.getSessionFactory() ;
-         
-         Session session = sessionFactory.openSession();
-         
-         session.beginTransaction();
-         
-         Song song = session.get(Song.class, 1);
-         
-         song.setSinger("Sukhivender");
-         song.setSongName("7 samunder");
-    	
-    	session.getTransaction().commit();
-    	
-    	session.close();
-    	
-    
-    	
-    	Session session2 = sessionFactory.openSession();
-    	
-    	session2.beginTransaction();
-    	
-    	session2.get(Song.class, 1);
-    	
-    	song.setSongName("samunder-updated123");
-    	
-    	session2.merge(song);
-    	
-    	session2.getTransaction().commit();
-    	
-    	
-    	
-    	
-    	
-         
-    	
-    }
+public class UpdadateApp {
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = Utility.getSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+		session.beginTransaction();
+
+		Song song = session.get(Song.class, 1);
+
+		song.setSinger("Sukhivender");
+		song.setSongName("7 samunder");
+
+		session.getTransaction().commit();
+
+		session.close();
+
+		Session session2 = sessionFactory.openSession();
+
+		session2.beginTransaction();
+
+		session2.get(Song.class, 1);
+
+		song.setSongName("samunder-updated1234");
+
+		session2.merge(song);
+
+		session2.getTransaction().commit();
+
+	}
 }
